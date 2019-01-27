6b91c16773afc8a9d0e6000f01ba1c8c235117dd
Harshil <harshils.how.to@gmail.com>
2019-01-27 11:01:01 +0000

    Completes tracking and separate csv file generation

--- goturnTracker.py
+++ goturnTracker.py
@@ -28,44 +28,61 @@ vid_writer = cv2.VideoWriter(outputFile,cv2.VideoWriter_fourcc('M','J','P','G'),
 # Exit if video not opened
 if not video.isOpened():
     print("Could not open video")
-    sys.exit()  
+    sys.exit()
+    
+tracker1 = cv2.TrackerGOTURN_create()
+tracker2 = cv2.TrackerGOTURN_create()
+okays1, frame = video.read()
+
     
 confThreshold = 0.1  #Confidence threshold
 
 inpWidth = 416
 inpHeight = 416
 net = setup_net()
-ok0, frame = video.read()
 
 data = get_points(frame,net,confThreshold)
 cents = data["boxes"]
 new_cents = get_centres(cents)
 simps = delete_repeats(new_cents)
 
-trackers = {}
-bboxes = {}
-okays = {}
 
-row = ["x0", "y0", "x1", "y1","x2", "y2", "x3", "y3"]
+row0 = ["x0", "y0"]
+row1 = ["x1", "y1"]
 
+with open('coords_player1.csv', 'a') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(row0)
+    row0 = []
+
+csvFile.close()
+
+
+with open('coords_player2.csv', 'a') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(row1)
+    row1 = []
+
+csvFile.close()
 
 #Initialize trackers for each box gathered
-for i in range(len(simps)):
-    trackers[i] = cv2.TrackerGOTURN_create()
-    bboxes[i] = tuple([simps[i][0] - 10, simps[i][1] - 10, 20, 20])
-    okays[i] = trackers[i].init(frame,bboxes[i])
+bboxes1 = tuple([simps[0][0] - 10, simps[0][1] - 10, 20, 20])
+okays1 = tracker1.init(frame,bboxes1)
+
+bboxes2 = tuple([simps[1][0] - 10, simps[1][1] - 10, 20, 20])
+okays2 = tracker2.init(frame,bboxes2)
 
 # Define a bounding box
 #bbox = cv2.selectROI(frame, False) #with user input
 
 
-with open('coords3_10.csv', 'a') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerow(row)
-
-for i in range(no_frames-1):
+while True:
     # Read a new frame 
-    okays[0], frame = video.read()
+    okays1, frame = video.read()
+    okays2, frame = video.read()
+    
+    if not okays1 or not okays2:
+        break
 
     cur_frame = int(video.get(cv2.CAP_PROP_POS_FRAMES))
     
@@ -74,36 +91,57 @@ for i in range(no_frames-1):
         cents = data["boxes"]
         new_cents = get_centres(cents)
         simps = delete_repeats(new_cents)
-        for i in range(len(simps)):
-            trackers[i] = cv2.TrackerGOTURN_create()
-            bboxes[i] = tuple([simps[i][0] - 10, simps[i][1] - 10, 20, 20])
-            okays[i] = trackers[i].init(frame,bboxes[i])
-
-
+        
+        ''' p1 = (int(bbox[0]), int(bbox[1]))
+        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        centre1 = '''
+        
+        
+        tracker1 = cv2.TrackerGOTURN_create()
+        bboxes1 = tuple([simps[0][0] - 10, simps[0][1] - 10, 20, 20])
+        okays1 = tracker1.init(frame,bboxes1)
+        
+        tracker2 = cv2.TrackerGOTURN_create()
+        bboxes2 = tuple([simps[1][0] - 10, simps[1][1] - 10, 20, 20])
+        okays2 = tracker2.init(frame,bboxes2)
+    
+    
     # Start timer
     timer = cv2.getTickCount()
 
     # Update tracker
-    for i in range(len(simps)):
-        okays[i], bboxes[i] = trackers[i].update(frame)
-    
-        # Tracking success
-    for i in range(len(simps)):
-        p1 = (int(bboxes[i][0]), int(bboxes[i][1]))
-        p2 = (int(bboxes[i][0] + bboxes[i][2]), int(bboxes[i][1] + bboxes[i][3]))
-        cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)  
-        centre = [(p1[0] + p2[0])/2 ] + [(p1[1] + p2[1])/2]
-        #print(centre)
-    
-    row.append(centre[0])
-    row.append(centre[1])
-    print(row)
-    row = []
+    okays1, bboxes1 = tracker1.update(frame)
+    okays2, bboxes2 = tracker2.update(frame)
 
-    with open('coords3_11.csv', 'a') as csvFile:
+    
+    # Tracking success
+    p1 = (int(bboxes1[0]), int(bboxes1[1]))
+    p2 = (int(bboxes1[0] + bboxes1[2]), int(bboxes1[1] + bboxes1[3]))
+    cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)  
+    centre1 = ((p1[0] + p2[0])/2, (p1[1] + p2[1])/2)
+    
+    p12 = (int(bboxes2[0]), int(bboxes2[1]))
+    p22 = (int(bboxes2[0] + bboxes2[2]), int(bboxes2[1] + bboxes2[3]))
+    cv2.rectangle(frame, p12, p22, (255,0,0), 2, 1)  
+    centre2 = ((p12[0] + p22[0])/2, (p12[1] + p22[1])/2)
+    
+    row0.append(centre1[0])
+    row0.append(centre1[1])
+    
+    row1.append(centre2[0])
+    row1.append(centre2[1])
+
+    with open('coords_player1.csv', 'a') as csvFile:
         writer = csv.writer(csvFile)
-        writer.writerow(row)
-        row = []
+        writer.writerow(row0)
+    row0 = []
+
+    csvFile.close()
+
+    with open('coords_player2.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(row1)
+    row1 = []
     
     csvFile.close()
         
