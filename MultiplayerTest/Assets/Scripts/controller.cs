45f4589642db1bfed655935c414ce5330e7d916a
Ferhaan Malek <ferhaanmalek@dyn3164-113.wlan.ic.ac.uk>
2019-01-27 02:51:53 +0000

    Tidies previous code

---
+++ MultiplayerTest/Assets/Scripts/controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+
+    public player PlayerPrefab;
+    public uint NumPlayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player aPlayer = Instantiate(PlayerPrefab, transform) as player;
+        //aPlayer.PlayerID = 1;
+
+        for(int p = 0; p < NumPlayers; p++)
+        {
+            player aPlayer = Instantiate(PlayerPrefab, transform) as player;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
